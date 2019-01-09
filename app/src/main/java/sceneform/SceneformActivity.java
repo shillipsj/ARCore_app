92994c3e1c5c9303481fd29ba6a3819cc7569997
shillipsj <46507719+shillipsj@users.noreply.github.com>
2019-01-09 07:56:43 +0700

    Add files via upload

---
+++ app/src/main/java/sceneform/SceneformActivity.java
@@ -0,0 +1,100 @@
+
+package sceneform;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+
+public class SceneformActivity extends AppCompatActivity {
+  private static final String TAG = SceneformActivity.class.getSimpleName();
+  private static final double MIN_OPENGL_VERSION = 3.0;
+
+  private ArFragment arFragment;
+  private ModelRenderable pdmodelRenderable;
+
+  @Override
+  @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+  // CompletableFuture requires api level 24
+  // FutureReturnValueIgnored is not valid
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (!checkIsSupportedDeviceOrFinish(this)) {
+      return;
+    }
+
+    setContentView(R.layout.activity_ux);
+    arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+    // When you build a Renderable, Sceneform loads its resources in the background while returning
+    // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
+    ModelRenderable.builder()
+        .setSource(this, R.raw.pdmodel)
+        .build()
+        .thenAccept(renderable -> pdmodelRenderable = renderable)
+        .exceptionally(
+            throwable -> {
+              Toast toast =
+                  Toast.makeText(this, "Unable to load 3d-model renderable", Toast.LENGTH_LONG);
+              toast.setGravity(Gravity.CENTER, 0, 0);
+              toast.show();
+              return null;
+            });
+
+    arFragment.setOnTapArPlaneListener(
+        (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+          if (pdmodelRenderable == null) {
+            return;
+          }
+
+          // Create the Anchor.
+          Anchor anchor = hitResult.createAnchor();
+          AnchorNode anchorNode = new AnchorNode(anchor);
+          anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+          // Create the transformable 3d-model and add it to the anchor.
+          TransformableNode pdmodel = new TransformableNode(arFragment.getTransformationSystem());
+            pdmodel.setParent(anchorNode);
+            pdmodel.setRenderable(pdmodelRenderable);
+            pdmodel.select();
+        });
+  }
+
+
+  public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+    if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
+      Log.e(TAG, "Sceneform requires Android N or later");
+      Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+      activity.finish();
+      return false;
+    }
+    String openGlVersionString =
+        ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+            .getDeviceConfigurationInfo()
+            .getGlEsVersion();
+    if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+      Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+      Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+          .show();
+      activity.finish();
+      return false;
+    }
+    return true;
+  }
+}
